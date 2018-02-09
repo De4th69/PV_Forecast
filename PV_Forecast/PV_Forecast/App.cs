56b17c7060017bf93b1cc5e37a83ad36ccacd093
Raphael Schindler <r.schindler@dgfrp.de>
2018-02-09 13:06:13 +0100

    Init
    Initial Commit

---
+++ PV_Forecast/PV_Forecast/App.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PV_Forecast
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new ContentPage
+            {
+                Content = new Label
+                {
+                    Text = "Hello World"
+                }
+            };
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+        }
+
+        protected override void OnSleep()
+        {
+            base.OnSleep();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+        }
+    }
+}
